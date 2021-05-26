6fdef0202ec6e4320708b4aa705145fc73314845
Yi Guan <tanyg1@outlook.sg>
2021-05-26 18:03:01 +0800

    minor fixes

--- accounts/views.py
+++ accounts/views.py
@@ -15,56 +15,63 @@ def dashboard(request):
 
 
 def register(request):
-    if request.method == "POST":
-        # Get form values
-        first_name = request.POST["first_name"]
-        last_name = request.POST["last_name"]
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-
-        # Check email
-        if User.objects.filter(email=email).exists():
-            messages.error(request, "Account already created, please login instead.")
-            return redirect("register")
-        elif User.objects.filter(username=username).exists():
-            messages.error(request, "Username has already been taken.")
-            return redirect("register")
-        else:
-            user = User.objects.create_user(
-                username=username,
-                first_name=first_name,
-                last_name=last_name,
-                email=email,
-                password=password,
-            )
-            # Login User after they register
-            auth.login(
-                request, user, backend="django.contrib.auth.backends.ModelBackend"
-            )
-            return redirect("dashboard")
-
-            # Redirect user to login page after registration
-            # user.save()
-            # return redirect("login")
+    if request.user.is_authenticated:
+        return redirect("dashboard")
     else:
-        return render(request, "accounts/register.html")
+        if request.method == "POST":
+            # Get form values
+            first_name = request.POST["first_name"]
+            last_name = request.POST["last_name"]
+            username = request.POST["username"]
+            email = request.POST["email"]
+            password = request.POST["password"]
+
+            # Check email
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "Account already created, please login instead.")
+                return redirect("register")
+            elif User.objects.filter(username=username).exists():
+                messages.error(request, "Username has already been taken.")
+                return redirect("register")
+            else:
+                user = User.objects.create_user(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    password=password,
+                )
+                # Login User after they register
+                auth.login(
+                    request, user, backend="django.contrib.auth.backends.ModelBackend"
+                )
+                return redirect("dashboard")
+
+                # Redirect user to login page after registration
+                # user.save()
+                # return redirect("login")
+        else:
+            return render(request, "accounts/register.html")
 
 
 def login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect("dashboard")
-        else:
-            messages.error(request, "Invalid credentials")
-            return redirect("login")
+    if request.user.is_authenticated:
+        print("logged in")
+        return redirect("dashboard")
     else:
-        return render(request, "accounts/login.html")
+        if request.method == "POST":
+            username = request.POST["username"]
+            password = request.POST["password"]
+
+            user = auth.authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+            else:
+                messages.error(request, "Invalid credentials")
+                return redirect("login")
+        else:
+            return render(request, "accounts/login.html")
 
 def transaction(request):
     return render(request, "accounts/transaction.html")
