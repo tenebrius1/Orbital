46d749fd8b5afb096df4f19a23d904fa361de34f
Yi Guan <tanyg1@outlook.sg>
2021-06-13 14:43:08 +0800

    switch to requests-html

--- scraping/amazon.py
+++ scraping/amazon.py
@@ -1,42 +1,7 @@
-from environs import Env
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-# Set up environ
-env = Env()
-env.read_env()
-
+from requests_html import HTMLSession, AsyncHTMLSession
 
 def checkAmazonPrice(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-software-rasterizer')
-    options.add_argument('--log-level=2')
-    options.binary_location = env.str('GOOGLE_CHROME_BIN')
-    driver = webdriver.Chrome(executable_path=env.str('CHROMEDRIVER_PATH'),options=options)
-    driver.get(url)
-    timeout = 1
-
-    try:
-        found = False
-        price = None
-        while not found:
-            try:
-                WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(
-                    (By.ID, 'priceblock_ourprice')))
-            except TimeoutException:
-                driver.refresh()
-                continue
-            price = driver.find_element(By.ID, 'priceblock_ourprice')
-            found = True
-        return price.text
-    except TimeoutException:
-        pass
-    finally:
-        driver.quit()
+    session = HTMLSession()
+    r = session.get(url)
+    price = r.html.find('#priceblock_ourprice', first=True)
+    return price.text
--- scraping/shopee.py
+++ scraping/shopee.py
@@ -1,42 +1,15 @@
-from environs import Env
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-# Set up environ
-env = Env()
-env.read_env()
+from requests_html import HTMLSession
 
 def checkShopeePrice(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-software-rasterizer')
-    options.add_argument('--log-level=2')
-    options.binary_location = env.str('GOOGLE_CHROME_BIN')
-    driver = webdriver.Chrome(executable_path=env.str('CHROMEDRIVER_PATH'),options=options)
-    driver.get(url)
-    timeout = 1
-
-    try:
-        found = False
-        price = None
-        while not found:
-            try:
-                WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(
-                    (By.CSS_SELECTOR, 'div[class="_3e_UQT"]')))
-            except TimeoutException:
-                driver.refresh()
-                continue
-            price = driver.find_element(
-                By.CSS_SELECTOR, 'div[class="_3e_UQT"]')
+    found = False
+    while not found:
+        try:
+            session = HTMLSession()
+            r = session.get(url)
+            r.html.render()
+            price_html = r.html.find('._3e_UQT', first=True)
+            price = price_html.text
             found = True
-        return price.text
-    except TimeoutException:
-        pass
-    finally:
-        driver.quit()
+            return price
+        except AttributeError:
+            pass
