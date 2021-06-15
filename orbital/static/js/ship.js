33a81fc7aec6d27b4ea783019e67772c13f39d1b
Yi Guan <tanyg1@outlook.sg>
2021-06-15 22:01:21 +0800

    update skeleton for shiptogether

--- orbital/static/js/ship.js
+++ orbital/static/js/ship.js
@@ -15,13 +15,27 @@
     })
 })()
 
-function myFunction() {
+function search() {
   // Declare variables
   var input, tr, table, i, group_name;
   input = document.getElementById('search').value.toLowerCase();
   table = document.getElementById('group_data');
   tr = table.getElementsByTagName('tr');
-  console.log(input)
-  console.log(table)
-  console.log(tr)
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toLowerCase().indexOf(input) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function redirect(group_name) {
+  location.href = "/accounts/ship/" + group_name;
 }
