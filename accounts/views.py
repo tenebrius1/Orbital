5416e28b92ab75d818b405033d8b1c39cb89f471
Chng Zi Hao <chngzihao@gmail.com>
2021-06-28 15:02:47 +0800

    minor fixes

--- accounts/views.py
+++ accounts/views.py
@@ -227,6 +227,9 @@ def joinGroup(request):
 def groupmainpage(request, group_name):
     group = Group.objects.get(group_name=group_name)
     data = Data.objects.filter(group_name=group_name)
+    # Ensures that the group is locked before allowing members to access this page
+    # if group.is_locked:
+    #     return redirect('grouplocked', group_name=group_name)
     tabledata = None
     if len(data) != 0:
         tabledata = zip(data[0].users, data[0].items, data[0].quantity, data[0].prices, data[0].urls)
@@ -268,8 +271,8 @@ def grouplocked(request, group_name):
     user_total = 0
 
     # Ensures that the group is locked before allowing members to access this page
-    # if not group.is_locked:
-    #     return redirect('groupmainpage', group_name=group_name)
+    if not group.is_locked:
+        return redirect('groupmainpage', group_name=group_name)
 
     tabledata = None
     if len(data) != 0:
